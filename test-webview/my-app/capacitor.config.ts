b0c153958a543544ee52b25adfbb1d158e532303
yamakenji24 <yamakenji24@gmail.com>
2022-10-02 17:33:57 +0900

    webview with capacitor and nextjs

---
+++ test-webview/my-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.mynextwebapp',
+  appName: 'my-next-webapp',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
