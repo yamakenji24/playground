e07a22dd5efa0d35f5fea345066abc7652002100
yamakenji24 <yamakenji24@gmail.com>
2022-12-27 16:42:26 +0900

    run storybook

---
+++ bun-vite-react/src/atoms/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+  args: {
+    title: 'button',
+    onClick: () => {},
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Default ボタン！！',
+};
+
+export const DifferentText = Template.bind({});
+DifferentText.args = {
+  title: '違うテキスト',
+};
