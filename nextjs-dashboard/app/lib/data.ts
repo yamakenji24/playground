15bac4b87e84217f0cf674b60103ca5d7a7d1299
yamakenji24 <yamakenji24@gmail.com>
2023-11-05 18:29:19 +0900

    server actions

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -175,6 +175,7 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoices by id.');
   }
 }
 
