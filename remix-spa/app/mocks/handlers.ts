6d42adec3dee7843b19080402e67cb9c4d47b9ba
yamakenji24 <yamakenji24@gmail.com>
2024-01-15 00:49:21 +0900

    clientAction

--- remix-spa/app/mocks/handlers.ts
+++ remix-spa/app/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw'
 import type { User } from '../model/user'
+import type { Todo } from '../model/todo'
 
 export const handlers = [
   http.get('/api/user', () => {
@@ -7,9 +8,17 @@ export const handlers = [
       firstName: 'John',
       lastName: 'Wick',
     } satisfies User)
-  })
+  }),
   // return 400 error
   // http.get('/api/user', () => {
   //   return HttpResponse.json(null, {status: 400, statusText: 'Bad Request'})
   // })
+  // return 200 in api/todo post
+  http.post('/api/todo', () => {
+    return HttpResponse.json({
+      id: '1',
+      title: 'new todo created',
+      completed: false,
+    } satisfies Todo)
+  })
 ]
--- remix-spa/app/routes/hoge/route.tsx
+++ remix-spa/app/routes/hoge/route.tsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, ClientActionFunctionArgs, Form, useActionData } from "@remix-run/react";
+import { Todo } from "~/model/todo";
 import { User } from "~/model/user";
 
 export async function clientLoader() {
@@ -7,14 +8,42 @@ export async function clientLoader() {
   return data;
 }
 
+export async function clientAction({ request }: ClientActionFunctionArgs) {
+  const body = await request.formData();
+  console.log(body);
+  console.log(body.get("todo"));
+
+  // Post to /api/todo
+  const response = await fetch("/api/todo", {
+    method: "POST",
+    body: JSON.stringify({
+      todo: body.get("todo"),
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  // return response
+  const data: Todo = await response.json();
+
+  return data;
+}
+
 export default function Index() {
   const user = useLoaderData<typeof clientLoader>();
-  console.log(user)
+  const newTodo = useActionData<typeof clientAction>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h2>Routes from hoge</h2>
       <User {...user} />
+      {/**create a todo form */}
+      <Form method="post">
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </Form>
+      {/**show the todo */}
+      {newTodo && <p>{newTodo.title}</p>}
     </div>
   );
 }
